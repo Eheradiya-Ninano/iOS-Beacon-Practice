5c70f6a190453cbefc7829eafed8705a9a425c9b
Jeremy Jeon <asura1983@naver.com>
2015-02-04 15:37:41 +0900

    Initial Commit

---
+++ BeaconPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconPractice
+//
+//  Created by WooKyun Jeon on 2015. 2. 4..
+//  Copyright (c) 2015년 WooKyun Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
